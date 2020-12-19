0353c50a3656c6449df3d233d28fa9ff77743931
Thiago Gonçalves de Jesus <thiagogdejesus@outlook.com.br>
2020-12-18 21:30:31 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,13 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=33):
+        self.age = idade
+        self.name = nome
+
     def cumprimentar(self):
         return 'Olá'
 
 
 if __name__ == '__main__':
     print(Pessoa().cumprimentar())
+    print(Pessoa('Thiago').name)
+    print(Pessoa().age)
