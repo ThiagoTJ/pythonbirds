8cd9c8440711020462ef916b3772900406dc69c3
Thiago Gonçalves de Jesus <thiagogdejesus@outlook.com.br>
2020-12-19 09:56:38 -0300

    Criado atributo complexo familia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=33):
+    def __init__(self, *familia, nome=None, idade=33):
         self.age = idade
         self.name = nome
+        self.family = list(familia)
 
     def cumprimentar(self):
         return 'Olá'
 
 
 if __name__ == '__main__':
-    print(Pessoa().cumprimentar())
-    print(Pessoa('Thiago').name)
-    print(Pessoa().age)
+    filha = Pessoa(nome='Laura')
+    mae = Pessoa(filha, nome='Suellen')
+    pai = Pessoa(filha, nome='Thiago')
+
+    print(Pessoa.cumprimentar(pai))
+    print(pai.name)
+    print(pai.age)
+    for filha in pai.family:
+        print(filha.name)
